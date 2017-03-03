58cfd2b622cff21b4471ac2175afc41a0163fb8c
root <root@DESKTOP-GSE79ED.eduroam.wireless.dtu.dk>
2017-03-03 11:29:28 +0100

    initial commit

---
+++ cmake-build-debug/CMakeFiles/NP_HA_FTP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NP_HA_FTP.dir/main.cpp.obj"
+  "NP_HA_FTP.pdb"
+  "NP_HA_FTP.exe"
+  "NP_HA_FTP.exe.manifest"
+  "libNP_HA_FTP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NP_HA_FTP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/NP_HA_FTP.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for NP_HA_FTP.
+# This may be replaced when dependencies are built.
