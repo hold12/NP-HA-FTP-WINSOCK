9f958aecc13c75c281b79a93cd6c51e6bb2624ca
Jsalonin <jornsalonin@gmail.com>
2017-03-03 13:37:02 +0100

    Update client.cpp
    inserted main.

--- src/client.cpp
+++ src/client.cpp
@@ -3,6 +3,15 @@ using namespace std;
 #include <winsock2.h>
 #include "Client.h"
 
+int main(){
+    std::cout<< "Hello, World" << std::endl;
+    Client c;
+    c.Connect(21,"130.226.195.126");
+    c.SendMsg("USER anonymous");
+    c.CloseCon();
+    return 0;
+}
+
 void Client::Connect(int port, char *adr)
 {
     WSAStartup(0x0101, &wlib);
