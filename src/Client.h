3dde639fdfb778dc5a57bb868e246647462a82b6
Troels Just Christoffersen <troelsjc@gmail.com>
2017-03-03 11:51:02 +0100

    Add files via upload
    up from codeblocks

---
+++ src/Client.h
@@ -0,0 +1,19 @@
+#ifndef KLIENT_H_INCLUDED
+#define KLIENT_H_INCLUDED
+
+
+#include <winsock2.h>
+class Client{
+public:
+ void Connect(int port, char *adr);
+ void SendMsg(char *pmsg);
+ void CloseCon();
+private:
+ int fd;
+ sockaddr_in saddr;
+ WSADATA wlib;
+ int nOk;
+};
+
+
+#endif // KLIENT_H_INCLUDED
