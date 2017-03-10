1957d1e3edcdb5429629a43cd1c9c44e6765cedb
trules <troelsjc@gmail.com>
2017-03-10 12:43:37 +0100

    Added LIST before RETR 2nd file and attempting to STOR

--- src/main.cpp
+++ src/main.cpp
@@ -42,10 +42,24 @@ int main() {
 	sscanf_s(c.RecvMsg(), "227 Entering Passive Mode (%d,%d,%d,%d,%d,%d).\r\n", &a1, &a2, &a3, &a4, &p1, &p2);
 	dataPort = (p1 * 256) + p2;
 
-	//Opening new data connection for 2nd file
+	//Opening new data connection for CWD and LIST
 	data.Connect(dataPort, ip_pointer);
 	c.SendMsg("CWD /pub/62501/HOWTOs/NAT-HOWTO\r\n", 33);
 	c.RecvMsg();
+	
+	c.SendMsg("LIST\r\n", 6);
+	c.RecvMsg();
+	data.RecvMsg();
+	c.RecvMsg();
+	data.CloseCon();
+	
+	//Entering Passive Mode again
+	c.SendMsg("PASV\r\n", 6);
+	sscanf_s(c.RecvMsg(), "227 Entering Passive Mode (%d,%d,%d,%d,%d,%d).\r\n", &a1, &a2, &a3, &a4, &p1, &p2);
+	dataPort = (p1 * 256) + p2;
+
+	//Opening new data connection to RETR 2nd file
+	data.Connect(dataPort, ip_pointer);
 	c.SendMsg("RETR NAT-HOWTO-3.html\r\n", 23);//bigger file of 1359 bytes
 	c.RecvMsg();
 	c.RecvMsg();
@@ -59,6 +73,12 @@ int main() {
 
 	//Opening new data connection to upload file
 	data.Connect(dataPort, ip_pointer);
+	c.SendMsg("CWD /\r\n", 7);
+	c.RecvMsg();
+	c.SendMsg("LIST\r\n", 6);
+	c.RecvMsg();
+	data.RecvMsg();
+	c.RecvMsg();
 	c.SendMsg("STOR CMakeLists.txt\r\n", 23);
 	c.RecvMsg();
 	data.CloseCon();
